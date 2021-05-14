fb34af6b1a9e87b8b311495dcac4b311db6a0089
Billie <46086149+chenxuefan@users.noreply.github.com>
2021-05-14 16:43:23 +0800

    Delete NCP directory

--- NCP/NCP疫情实时动态（腾讯）/NCP疫情趋势分析（卫健委）/deploy.sh
+++
@@ -1,7 +0,0 @@
-git init
-git remote rm origin
-git remote add origin https://github.com/chenxuefan/NCP-NHC.git
-git add .
-git commit -m "update"
-git pull origin master
-git push -u origin +master
